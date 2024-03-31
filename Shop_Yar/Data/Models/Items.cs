da4399d1dd4897f1cc5fefdb3d68d69d7d3de195
Ефим <fimakrut@mail.ru>
2024-03-31 16:37:35 +0500

    Исправленные ошибки

--- Shop_Yar/Data/Models/Items.cs
+++ Shop_Yar/Data/Models/Items.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Shop_Yar.Data.Models
 {
-    public class items
+    public class Items
     {
         public int Id { get; set; }
         public string Name { get; set; }
